e20273adc28cefcdf518122cf7d4fdfe659d19a7
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-03-12 14:41:15 +0200

    Navbar changes add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
+import Onesheet from "../assets/onesheetLogo.png";
+
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleNav = () => {
@@ -8,27 +10,30 @@ const Navbar = () => {
   };
 
   return (
-    <div className="w-full h-[90px] bg-white">
+    <div className="w-full h-[80px] bg-[#56AC8B]">
       <div className="max-w-[1240px] mx-auto px-4 flex justify-between items-center h-full">
         <div>
-          <h1 className="text-[var(--primary-blue)]">ONESHEET</h1>
+          <h1 className="text-[var(--primary-blue)]">
+            <img src={Onesheet} alt="Onesheet Logo" className="w-[210px]" />
+          </h1>
         </div>
         <div className="hidden md:flex">
-          <ul className="flex text-[var(--primary-blue)] items-center">
-            <li>About</li>
-            <li>Features</li>
-            <li>Pricing</li>
-            <li>Contact</li>
-            <button className="ml-4">Start free trial</button>
+          <ul className="flex text-white items-center">
+            <li className="m-5">Features</li>
+            <li className="m-5">Why Us</li>
+            <li className="m-5">Pricing</li>
+            <li className="m-5">Companies</li>
+            <li className="m-5">Contact</li>
+            {/* <button className="ml-4">Start free trial</button> */}
           </ul>
         </div>
 
         {/* Hamburger Menu */}
         <div className="block md:hidden" onClick={handleNav}>
           {nav ? (
-            <AiOutlineClose size={30} className="text-[var(--primary-blue)]" />
+            <AiOutlineClose size={30} className="text-white" />
           ) : (
-            <AiOutlineMenu size={30} className="text-[var(--primary-blue)]" />
+            <AiOutlineMenu size={30} className="text-white" />
           )}
         </div>
 
@@ -36,16 +41,17 @@ const Navbar = () => {
         <div
           className={
             nav
-              ? "w-full bg-black text-white absolute top-[90px] left-0 flex justify-center text-center"
+              ? "w-full bg-[#56AC8B] text-white absolute top-[90px] left-0 flex justify-center text-center"
               : "absolute left-[-100%]"
           }
         >
           <ul>
-            <li className="text-2xl">About</li>
             <li className="text-2xl">Features</li>
+            <li className="text-2xl">Why Us</li>
             <li className="text-2xl">Pricing</li>
+            <li className="text-2xl">Companies</li>
             <li className="text-2xl">Contact</li>
-            <button className="m-8">Start free trial</button>
+            {/* <button className="m-8">Start free trial</button> */}
           </ul>
         </div>
       </div>
