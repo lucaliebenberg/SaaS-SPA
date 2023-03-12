4f3fb9672768cc8a2f781c2de1b2fb69e0f68976
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-03-12 20:10:25 +0200

    Hero desktop complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,16 +5,16 @@ import Product1 from "../assets/product1.png";
 const Hero = () => {
   return (
     <div className="w-full h-[92vh] bg-[#56AC8B]">
-      <div className="w-full h-full flex flex-row justify-between align-middle pt-[5rem]">
+      <div className="w-full h-full flex flex-row justify-between align-middle pt-[10rem]">
         <div className="flex flex-col pt-8 pl-[6rem]">
           <h1 className="text-[48px] max-w-[530px] text-white font-bold text-left leading-tight mb-8">
             We help you to grow your business to the next level
           </h1>
           <div className="flex flex-row">
-            <button className="w-[141px] h-[50px] rounded-[5px] bg-white text-[#56AC8B] mr-3">
+            <button className="w-[141px] h-[50px] rounded-[5px] bg-white text-[#56AC8B] mr-3 ">
               Get Started
             </button>
-            <button className="w-[141px] h-[50px] rounded-[5px] bg-[#56AC8B] border-white border-2 text-white">
+            <button className="w-[141px] h-[50px] rounded-[5px] bg-[#56AC8B] border-white border-2 text-white ">
               Learn More
             </button>
           </div>
