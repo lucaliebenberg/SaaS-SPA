2a9691935fb08717e97c46bda39a62f36ae4cf8a
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-03-12 19:01:45 +0200

    Hero assets add + project assets add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
-function Hero() {
-  return <div>Hero</div>;
-}
+import Product1 from "../assets/product1.png";
+
+const Hero = () => {
+  return (
+    <div className="w-full h-[90vh] bg-[#56AC8B]">
+      <div className="w-full h-full flex flex-row justify-between">
+        <div className="flex flex-col">
+          <h1 className="text-[52px] max-w-[508px] text-white font-bold text-left">
+            We help you to grow your business to the next level
+          </h1>
+          <div className="flex flex-row">
+            <button className="w-[141px] h-[50px] rounded-[5px] bg-white text-[#56AC8B]">
+              Get Started
+            </button>
+            <button className="w-[141px] h-[50px] rounded-[5px] bg-[#56AC8B] border-white border-2 text-white">
+              Learn More
+            </button>
+          </div>
+        </div>
+        <div>
+          <img src={Product1} alt="" className="w-full h-full" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
