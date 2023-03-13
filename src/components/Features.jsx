b18bd7f16f9b0210bb26e3d387ef0acdd66a53f3
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-03-13 06:56:46 +0200

    Feature add (icon alignment needed)

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -7,19 +7,21 @@ import Feature3 from "../assets/Icon-2.png";
 const Features = () => {
   return (
     <div className="w-full h-[90vh]">
-      <h1 className="text-[#8C8C8C] font-bold text-[48px]">Our Features</h1>
+      <h1 className="text-[#8C8C8C] font-bold text-[40px] p-[5rem]">
+        Our Features
+      </h1>
 
-      <div className="flex flex-row w-full h-full">
+      <div className="flex flex-row justify-between  p-[5rem] h-full">
         <div className="flex flex-col">
           <img
             src={Feature1}
             alt="Feature 1 icon"
-            className="h-[40px] w-[40px] justify-center"
+            className="h-[40px] w-[40px]"
           />
-          <h2 className="text-[#56AC8B] font-bold text-[28px]">
+          <h2 className="text-[#56AC8B] font-bold text-[28px] justify-center text-center">
             Simple Accounting
           </h2>
-          <p className="text-[18px] text-[#ABABAB] text-center max-w-[277px]">
+          <p className="text-[18px] text-[#ABABAB] justify-center text-center max-w-[277px]">
             Manage expenses, and generate detailed reports with a few clicks.
           </p>
         </div>
@@ -31,10 +33,10 @@ const Features = () => {
               alt="Feature 2 icon"
               className="h-[40px] w-[40px]"
             />
-            <h2 className="text-[#56AC8B] font-bold text-[28px]">
+            <h2 className="text-[#56AC8B] font-bold text-[28px] justify-center text-center">
               Powerful Features
             </h2>
-            <p className="text-[18px] text-[#ABABAB] text-center max-w-[277px]">
+            <p className="text-[18px] text-[#ABABAB] justify-center text-center max-w-[277px]">
               Financial Tracking, Expense Management and Ai Automation
             </p>
           </div>
@@ -46,10 +48,10 @@ const Features = () => {
               alt="Feature 3 icon"
               className="h-[40px] w-[40px]"
             />
-            <h2 className="text-[#56AC8B] font-bold text-[28px]">
+            <h2 className="text-[#56AC8B] font-bold text-[28px] justify-center text-center">
               Expert AI Analytics
             </h2>
-            <p className="text-[18px] text-[#ABABAB] text-center max-w-[277px]">
+            <p className="text-[18px] text-[#ABABAB] justify-center text-center max-w-[277px]">
               Ability to streamline finances and gain valuable insights.
             </p>
           </div>
