1cc63b92d029ea7b9af7cb469dd668adf5a42c76
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-03-13 12:00:55 +0200

    minor Footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
-function Footer() {
-  return <div>Footer</div>;
-}
+const Footer = () => {
+  return (
+    <div className="w-full h-[40vh] bg-white">
+      <div className="flex flex-col align-middle justify-center">
+        <p className="text-[#909090] font-semibold text-[12px] text-center">
+          LIKE WHAT YOU SEE?
+        </p>
+        <h1 className="text-[#55AA8A] font-semibold text-[28px] text-center">
+          Let's work together
+        </h1>
+        <p className="max-w-[384px] text-black text-center text-[16px]">
+          If you’re looking for a kick-ass software company, we’re here to help!
+        </p>
+        <button className="w-[134px] h-[40px] rounded-[5px] bg-[#55AA8A] text-white">
+          Get in touch
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
