79790bce2867c03a1ae610ab806ce17fac352e18
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-03-13 07:21:00 +0200

    WhyUs add

--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-function WhyUs() {
-  return <div>WhyUs</div>;
-}
+import Product2 from "../assets/product2.png";
+
+const WhyUs = () => {
+  return (
+    <div className="w-full h-[90vh] bg-[#56AC8B]">
+      <div className="p-[5rem]">
+        <h1 className="text-[40px] font-bold text-white">Why Us</h1>
+        <p className="max-w-[1141px] text-[23px] text-[#FDFDFD] pt-[3rem]">
+          Onesheet is an intuitive online accounting software that provides
+          users with an easy way to track financial information, manage
+          expenses, and generate detailed reports with expert AI analytics.
+          Modern accounting software is powered by AI, giving businesses the
+          ability to streamline their finances and gain valuable insights from
+          their financial data, utilising predictive analytics, natural language
+          processing and being highly configurable.
+        </p>
+      </div>
+      <div className="flex justify-center align-middle ml-auto mr-auto pt-[5rem]">
+        <img src={Product2} alt="Onesheet product 2" className="" />
+      </div>
+    </div>
+  );
+};
 
 export default WhyUs;
