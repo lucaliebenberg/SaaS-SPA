d976f3058579d1b0cc186cb3394db4e417625a96
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-03-13 12:52:21 +0200

    hover changes made to Pricing component

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -5,25 +5,25 @@ import Pricing2 from "../assets/pricing2.png";
 
 const Pricing = () => {
   return (
-    <div className="w-full h-[100vh] bg-[#56AC8B]">
+    <div className="w-full h-[101vh] bg-[#56AC8B]">
       <div>
-        <h1 className="text-[40px] font-bold text-center pt-3">
+        <h1 className="text-[40px] font-bold text-center pt-3 text-white">
           Simple pricing
         </h1>
-        <p className="text-[24px] font-light text-center pt-3">
+        <p className="text-[18px] font-light text-center pt-3 text-white">
           Choose the package that aligns with your business needs
         </p>
       </div>
-      <div className="w-[70vw] flex flex-row justify-around align-middle ml-auto mr-auto pt-4">
+      <div className="w-[70vw] flex flex-row justify-around align-middle ml-auto mr-auto pt-[3rem] pb-8">
         <img
           src={Pricing2}
           alt="Pricing option 1"
-          className="h-[520px] w-auto"
+          className="h-[520px] w-auto hover:cursor-pointer hover:scale-105"
         />
         <img
           src={Pricing1}
           alt="Pricing option 2"
-          className="h-[520px] w-auto"
+          className="h-[520px] w-auto hover:cursor-pointer hover:scale-105"
         />
       </div>
     </div>
