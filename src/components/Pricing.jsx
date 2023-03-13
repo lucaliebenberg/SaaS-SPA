b3c73fc79982e0914031952591a308fb5eb1a125
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-03-13 08:24:18 +0200

    Pricing add

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
-function Pricing() {
-  return <div>Pricing</div>;
-}
+import Pricing1 from "../assets/pricing1.png";
+import Pricing2 from "../assets/pricing2.png";
+
+const Pricing = () => {
+  return (
+    <div className="w-full h-[100vh] bg-[#56AC8B]">
+      <div>
+        <h1 className="text-[40px] font-bold text-center pt-3">
+          Simple pricing
+        </h1>
+        <p className="text-[24px] font-light text-center pt-3">
+          Choose the package that aligns with your business needs
+        </p>
+      </div>
+      <div className="flex flex-row justify-between align-middle ml-auto mr-auto">
+        <img
+          src={Pricing2}
+          alt="Pricing option 1"
+          className="h-[520px] w-auto"
+        />
+        <img
+          src={Pricing1}
+          alt="Pricing option 2"
+          className="h-[520px] w-auto"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Pricing;
