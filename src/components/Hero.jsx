5fb4694db02bedb560c0fe737e115c0c3d1b03e2
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-03-16 08:55:36 +0200

    framer motion added to Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,31 +6,40 @@ import { slideIn, fadeIn, textVariant } from "../utils/motion";
 
 const Hero = () => {
   return (
-    <motion.div
-      initial="initial"
-      animate="animate"
-      id="hero"
-      className="w-full h-[92vh] bg-[#56AC8B]"
-    >
+    <motion.div id="hero" className="w-full h-[92vh] bg-[#56AC8B]">
       <div className="w-full h-full flex flex-row justify-between align-middle pt-[6rem] lg:pt-[10rem]">
-        <motion.div
-          variants={fadeIn("", "", 0.1, 1)}
-          className="flex flex-col pl-[3.3rem] lg:pl-[6rem] lg:pt-8"
-        >
-          <h1 className="text-[38px] max-w-[220px] lg:max-w-[508px] text-white font-bold text-left leading-tight mb-8 lg:text-[52px]">
+        <motion.div className="flex flex-col pl-[3.3rem] lg:pl-[6rem] lg:pt-8">
+          <motion.h1
+            initial={{ x: "-200px" }}
+            animate={{ x: 0 }}
+            transition={{ duration: 1 }}
+            className="text-[38px] max-w-[220px] lg:max-w-[508px] text-white font-bold text-left leading-tight mb-8 lg:text-[52px]"
+          >
             We help you to grow your business to the next level
-          </h1>
+          </motion.h1>
           <div className="flex flex-row">
-            <button className="w-[115px] h-[41px] rounded-[5px] bg-[#56AC8B] mr-3 border-white border-2 text-white  lg:w-[141px] lg:h-[50px] hover:scale-105 hover:cursor-pointer">
+            <motion.button
+              initial={{ x: "-200px" }}
+              animate={{ x: 0 }}
+              transition={{ duration: 3 }}
+              className="w-[115px] h-[41px] rounded-[5px] bg-[#56AC8B] mr-3 border-white border-2 text-white  lg:w-[141px] lg:h-[50px] hover:scale-105 hover:cursor-pointer"
+            >
               Learn More
-            </button>
-            <button className="w-[115px] h-[41px] rounded-[5px] bg-white text-[#56AC8B] lg:w-[141px] lg:h-[50px] hover:scale-105 hover:cursor-pointer">
+            </motion.button>
+            <motion.button
+              initial={{ x: "-200px" }}
+              animate={{ x: 0 }}
+              transition={{ duration: 2 }}
+              className="w-[115px] h-[41px] rounded-[5px] bg-white text-[#56AC8B] lg:w-[141px] lg:h-[50px] hover:scale-105 hover:cursor-pointer"
+            >
               Get Started
-            </button>
+            </motion.button>
           </div>
         </motion.div>
         <motion.div
-          variants={slideIn("right", "tween", 0.2, 1)}
+          initial={{ x: "500px" }}
+          animate={{ x: 0 }}
+          transition={{ duration: 1 }}
           className="sm: hidden md:hidden lg:inline-flex ml-[5rem] "
         >
           <img src={Product1} alt="" className="w-full h-full" />
